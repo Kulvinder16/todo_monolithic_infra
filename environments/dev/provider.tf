200efc85518b7d3ea7ef93f7e8c315730777c60a
Kulvinder Singh <kulvindersinghmatharoo@gmail.com>
2025-12-17 19:22:29 +0530

    new azure account

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rgkulvinder"
-    storage_account_name = "stgkulvinder1"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "stgkulvinder"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "container1"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
