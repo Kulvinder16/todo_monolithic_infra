96691b00f9c1b0d3139fd1126e625d60d0b0bf54
Kulvinder Singh <kulvindersinghmatharoo@gmail.com>
2025-12-23 00:00:51 +0530

    added sql database with for_each

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.49.0"
+      version = "4.57.0"
     }
   }
   backend "azurerm" {
     resource_group_name  = "rgkulvinder"
-    storage_account_name = "stgkulvinder"         # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "stgkulvinder"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "container1"            # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
